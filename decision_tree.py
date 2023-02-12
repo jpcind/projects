fa6ef2102b7dfb7d6b4f66d92de4f940303cbacd
jpcind <108848785+jpcind@users.noreply.github.com>
2023-02-11 16:46:07 -0800

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -16,6 +16,8 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 
 db = []
+X = []
+Y = []
 
 # reading the data in a csv file
 with open('contact_lens.csv', 'r') as csvfile:
@@ -29,27 +31,51 @@ with open('contact_lens.csv', 'r') as csvfile:
 # For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
 # so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
 # --> add your Python code here
-X = [[1, 1, 2, 2],
-     [2, 1, 2, 1],
-     [3, 1, 2, 1],
-     [3, 1, 2, 1],
-     [2, 1, 1, 1],
-     [1, 1, 1, 1],
-     [1, 2, 2, 2],
-     [3, 1, 1, 2],
-     [2, 2, 2, 2],
-     [1, 1, 1, 2]]
+
+for i in range(len(db)):
+    temp_list = []
+    for j in range(len(db)):
+        if j == 0:
+            if db[i][j] == 'Young':
+                temp_list.append(1)
+            elif db[i][j] == 'Presbyopic':
+                temp_list.append(2)
+            else:
+                temp_list.append(3)
+        if j == 1:
+            if db[i][j] == 'Myope':
+                temp_list.append(1)
+            else:
+                temp_list.append(2)
+        if j == 2:
+            if db[i][j] == 'Yes':
+                temp_list.append(1)
+            else:
+                temp_list.append(2)
+        if j == 3:
+            if db[i][3] == 'Normal':
+                temp_list.append(1)
+            else:
+                temp_list.append(2)
+    X.append(temp_list)
 
 # transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1,
 # No = 2, so Y = [1, 1, 2, 2, ...]
 # --> add your Python code here
-Y = [2, 2, 2, 1, 1, 1, 2, 2, 2, 1]
+
+for i in range(len(db)):
+    if db[i][len(db[i])-1] == 'Yes':
+        Y.append(1)
+    else:
+        Y.append(2)
 
 # fitting the decision tree to the data
-clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=4)
+clf = tree.DecisionTreeClassifier(criterion='entropy')
 clf = clf.fit(X, Y)
 
 ## plotting the decision tree
-tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes', 'No'], filled=True,
-               rounded=True)
+tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'],
+               class_names=['Yes', 'No'],
+               filled=True, rounded=True)
 plt.show()
+
